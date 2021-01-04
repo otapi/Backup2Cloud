8f6ace80412c87c782f94056a287b688adf63038
otapi <otapi@protonmail.ch>
2021-01-04 09:21:44 +0100

    separated gdrive

--- Backup2Cloud/__main__.py
+++ Backup2Cloud/__main__.py
@@ -1,74 +1,13 @@
 import configparser
 import os
 import tempfile
-import pickle
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.http import MediaFileUpload
-
+from .gdrive import GDrive
 
 def zipencrypt(folder):
     pass
 
 def unzipdecrypt(folder):
     pass
-
-def gdrive(credentialfile):
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                credentialfile, ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.file'])
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('drive', 'v3', credentials=creds, cache_discovery=False)
-    
-    return service
-
-def listfiles(credentialfile):
-    service = gdrive(credentialfile)
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-
-def downloadfile(credentialfile, file):
-    pass
-
-def uploadfile(credentialfile, file):
-    if not os.path.isfile(file):
-        raise Exception(f"The file to upload does not exists: {file}")
-    service = gdrive(credentialfile)
-    media = MediaFileUpload(file, mimetype='application/zip', resumable=True)
-
-    
-    request = service.files().create(media_body=media, body={'name': os.path.basename(file)})
-    response = None
-    while response is None:
-        status, response = request.next_chunk()
-        if status:
-            print("Uploaded %d%%." % int(status.progress() * 100))
-        logging.info(f"Upload Complete!")
-    service.close()
 
 def Main():
     logging.info(f"Backup2Cloud")
@@ -78,29 +17,29 @@ def Main():
     logging.info(f"Using config {configfile}")
     config = configparser.ConfigParser()
     config.read(configfile)
-    with tempfile.TemporaryDirectory() as tempdir:
-        for cloudplace in config.sections():
-            logging.info(f"Process {cloudplace}")
-            credentialfile = None
-            zipfile = None
+    for cloudplace in config.sections():
+        with GDrive() as gd:
+            with tempfile.TemporaryDirectory() as tempdir:
+                logging.info(f"Process {cloudplace}")
+                zipfile = None
 
-            for (key, val) in config.items(cloudplace):
-                if key=="credentials":
-                    credentialfile = val
-                    continue
-                elif key=="folder":
-                    folder = val
-                    zipfile = os.path.join(tempdir, folder)
-                    zipencrypt(folder=zipfile)
-                elif key=="file":
-                    zipfile = val
+                for (key, val) in config.items(cloudplace):
+                    if key=="credentials":
+                        gd.connect(val)
+                        continue
+                    elif key=="folder":
+                        folder = val
+                        zipfile = os.path.join(tempdir, folder)
+                        zipencrypt(folder=zipfile)
+                    elif key=="file":
+                        zipfile = val
 
-                if not credentialfile:
-                    raise Exception(f"Credentials as missing from INI for {cloudplace}")
-                
-                if not zipfile:
-                    raise Exception(f"Either the folder or file is missing from INI for {cloudplace}")
-                uploadfile(credentialfile=credentialfile, file=zipfile)
+                    if not gd.isconnected():
+                        raise Exception(f"Credentials are missing from INI for {cloudplace}")
+                    
+                    if not zipfile:
+                        raise Exception(f"Either the folder or file is missing from INI for {cloudplace}")
+                    gd.uploadfile(filepath=zipfile)
 
 ### set up logging
 import logging, sys, socket
---
+++ Backup2Cloud/gdrive.py
@@ -0,0 +1,108 @@
+import os
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload
+import logging
+
+# Class to upload, download and list files on Google Drive.
+class GDrive():
+    def __init__(self, credentialfile=None):
+        self.service = None
+        if credentialfile:
+            self.connect(credentialfile)
+
+    def connect(self, credentialfile):
+        if not self.service:
+            logging.info(f"Already connected to GDrive. Avoid connecting multiple times by the same GDrive object.")
+
+        logging.info(f"Connect to GDrive using {credentialfile}...")
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentialfile, ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.file'])
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('drive', 'v3', credentials=creds, cache_discovery=False)
+        logging.info(f"Connected!")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.service:
+            self.service.close()
+
+    def listfiles(self):
+        if not self.service:
+            raise Exception("Call Connect before start using GDrive")
+        results = self.service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        return items
+
+    def printfiles(self):
+        items = self.listfiles()
+        if not items:
+            print('No files found.')
+        else:
+            print('Files:')
+            for item in items:
+                print(u'{0} ({1})'.format(item['name'], item['id']))
+
+    # Check if file exists with exact name on GDrive. Returns the id of the file if yes.
+    def fileexists(self, filename):
+        items = self.listfiles()
+        if items:
+            for item in items:
+                if item['name'] == filename:
+                    return item['id']
+        return None
+
+    # Upload specified local filepath to GDrive. Delete already existing one if overwrite=True
+    def uploadfile(self, filepath, overwrite=True):
+        if not self.service:
+            raise Exception("Call Connect before start using GDrive")
+        if not os.path.isfile(filepath):
+            raise Exception(f"The local file to upload does not exists: {filepath}")
+
+        logging.info(f"Upload started for {filepath}...")
+        media = MediaFileUpload(filepath, mimetype='application/zip', resumable=True)
+        request = self.service.files().create(media_body=media, body={'name': os.path.basename(filepath)})
+        response = None
+        while response is None:
+            status, response = request.next_chunk()
+            if status:
+                print("Uploaded %d%%." % int(status.progress() * 100))
+            logging.info(f"Upload Complete!")
+
+    # Download the specified file from GDrive to the specified local folder. Returns the local filepath.
+    def downloadfile(self, filename, localfolder):
+        if not self.service:
+            raise Exception("Call Connect before start using GDrive")
+
+    # Return True if already connected
+    def isconnected(self):
+        if self.service:
+            return True
+        else:
+            return False
+
+
+
+
+

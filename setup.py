a3d566b0eedc7c99e4f95cd3f9420eec6b824fc7
otapi <otapi@protonmail.ch>
2021-01-16 21:10:26 +0100

    print cloudplace name when credentials may needed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Backup2Cloud",
-    version="1.0.11",
+    version="1.0.12",
     author="otapi",
     description="Backup specific folders to and upload to a cloud provider",
     long_description=long_description,
